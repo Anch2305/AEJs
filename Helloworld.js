914233326dfff981c700b93ef3c707b302e8947f
anch2305 <anchana.rajamathe@gmail.com>
2016-04-18 16:39:39 -0700

    Copied node hello world example

--- Helloworld.js
+++ Helloworld.js
@@ -1,14 +1,18 @@
-// Load the http module to create an http server.
-var http = require('http');
+var express = require('express');
+var app = express();
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
-
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
